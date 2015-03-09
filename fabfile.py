6f231be2ae77e3c3c35dc89f862da6bb6234a5c4
jayfo <jayfo.github@gmail.com>
2015-03-09 13:58:08 -0700

    Clean and Migrate Fabric

--- fabfile.py
+++ fabfile.py
@@ -1,24 +1,11 @@
 import fabric
 import fabric.api
 
-# I have disabled all of this because Daniel is also pushing.as
-#
 # For now, we'll specify host/user interactively.
 #
-# Eventually I think we want dynamically-set host lists from a settings file:
+# Run `fab deploy`, you'll be prompted for a host string.
 #
-# http://docs.fabfile.org/en/latest/usage/execution.html
-#
-# configs = {
-#     'jfogarty': {'hosts': ['barb.cs.washington.edu'], 'user': 'jfogarty'},
-#     'depstein': {'hosts': ['bicycle.cs.washington.edu'], 'user': 'depstein'}
-# }
-#
-# # Configure our host and user according to the parameter
-# fabric.api.env.hosts = configs[param_config]['hosts']
-# fabric.api.env.user = configs[param_config]['user']
-#
-# fabric.api.env.hosts = ['jfogarty@barb.cs.washington.edu']
+# Enter a string like `jfogarty@barb.cs.washington.edu`
 
 
 def build():
@@ -30,25 +17,27 @@ def deploy():
     fabric.api.local('jekyll build --config _config.yml')
 
     # Ensure the server has our staging directory
-    fabric.api.run('mkdir -p ~/fabric_staging/web-cse440-au14/')
+    fabric.api.run('mkdir -p ~/fabric_staging/web-cse441-sp15/')
 
     # Ensure the staging directory is empty
-    fabric.api.run('rm -rf ~/fabric_staging/web-cse440-au14/*')
+    fabric.api.run('rm -rf ~/fabric_staging/web-cse441-sp15/*')
 
     # Push up to the server staging directory
-    fabric.api.put('_site/*', '~/fabric_staging/web-cse440-au14/')
+    fabric.api.put('_site/*', '~/fabric_staging/web-cse441-sp15/')
 
     # And sync into the deployment directory
-    fabric.api.run('rsync -r -c --delete ~/fabric_staging/web-cse440-au14/ /cse/web/courses/cse440/14au/')
+    fabric.api.run('rsync -r -c --delete ~/fabric_staging/web-cse441-sp15/ /cse/web/courses/cse441/15sp/')
 
     # Set group write permissions
-    fabric.api.run('chmod -R g+w /cse/web/courses/cse440/14au/')
+    fabric.api.run('chmod -R g+w /cse/web/courses/cse441/15sp/')
+
 
 def serve():
     fabric.api.local('jekyll serve --config _config.yml,_config-dev.yml --watch --force_polling')
 
 
-# We cannot have a test because Fabric requires Python 2.7 but our tests use Python 3.4
+# We cannot have a test because Fabric requires Python 2.7 but our tests use Python 3.4.
+# It might be possible to fix this by moving to invoke, the Python 3 descendant of Fabric.
 #
 # def test():
 #    fabric.api.local('nosetests')
